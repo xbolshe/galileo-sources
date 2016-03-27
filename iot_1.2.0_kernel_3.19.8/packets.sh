1ca48feeaeb591590c9f769c66754098cdf8d6d0
Eugene Bolshakov <pub@relvarsoft.com>
2016-03-27 12:39:41 +0300

    Add xterm Signed-off-by: Eugene Bolshakov <pub@relvarsoft.com>

--- iot_1.2.0_kernel_3.19.8/packets.sh
+++ iot_1.2.0_kernel_3.19.8/packets.sh
@@ -13,6 +13,7 @@ apt-get install diffstat gawk chrpath file
 apt-get install build-essential uuid-dev iasl
 apt-get install subversion nasm autoconf
 apt-get install gnu-efi qemu libsdl1.2-dev
+apt-get install xterm
 
 ###############################################
 ### uncomment it to use default git account ###
